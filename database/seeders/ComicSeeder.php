57efa2374bc4733b971c3dcc26a60549592f126b
Luca <lucafagiolini@live.it>
2024-04-14 19:50:33 +0200

    correct for each for the ComicSeeder, is creating all the informatins in the database correctly

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -13,16 +13,28 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        $newComic = new Comic();
 
-        $newComic->title = "Action Comics #1000: The Deluxe Edition";
-        $newComic->description = "The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!";
-        $newComic->thumb = "https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY";
-        $newComic->price = "$19.99";
-        $newComic->series = "Action Comics";
-        $newComic->sale_date = "2018-10-02";
-        $newComic->type = "comic book";
+        $comics = config('comics');
+        // $this->command->info(print_r($comics));
 
-        $newComic->save();
+        foreach ($comics as $comic) {
+
+            $newComic = new Comic();
+
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+
+
+            $newComic->artists = implode(", ", $comic['artists']);
+            $newComic->writers = implode(", ", $comic['writers']);
+
+            $newComic->save();
+        }
     }
 }
